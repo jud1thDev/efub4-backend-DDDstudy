48d8576840f7f4591588fabb3c4ce6009144fdf6
judith <141399892+jud1thDev@users.noreply.github.com>
2024-03-12 00:29:09 +0900

    Update Money.java

--- CH1/Money.java
+++ CH1/Money.java
@@ -2,7 +2,7 @@ package CH1;
 
 // 이전 코드에선 Orderline의 price, amounts가 int를 사용하고 있었다.
 // 돈을 의미하는 Money 타입을 만들어 코드 이해를 더 명확히 돕자.
-// 정수 타입 연산 -? 돈 계산 으로의 코드의미 변경
+// 정수 타입 연산 -> 돈 계산 으로의 코드의미 변경
 public class Money {
     private int value;
 

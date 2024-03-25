da1b757864d1c8462ab8874a21271ec2f0156994
judith <8w8u8@naver.com>
2024-03-25 16:27:54 +0900

    ~5.8

--- src/main/java/ewhabackendDDDstudy/member/query/MemberDataSpecs.java
+++ src/main/java/ewhabackendDDDstudy/member/query/MemberDataSpecs.java
@@ -1,6 +1,10 @@
 package ewhabackendDDDstudy.member.query;
 
+import ewhabackendDDDstudy.common.jpa.SpecBuilder;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.domain.Specification;
+
+import java.util.List;
 
 public class MemberDataSpecs {
 
@@ -11,4 +15,12 @@ public class MemberDataSpecs {
     public static Specification<MemberData> nameLike(String keyword) {
         return (root, query, cb) -> cb.like(root.<String>get("name"), keyword + "%");
     }
+
+    Specification<MemberData> spec = SpecBuilder.builder(MemberData.class)
+            .ifTrue(searchRequest.isOnlyNotBlocked(),
+                    () -> MemberDataSpecs.nonBlocked())
+            .ifHasText(searchRequest.getName(),
+                    name -> MemberDataSpecs.nameLike(searchRequest.getName()))
+            .toSpec();
+    List<MemberData> result = memberDataDao.findAll(spec, PageRequest.of(0, 5));
 }
